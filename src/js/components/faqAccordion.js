84c5299b758b85b9484d6a32f45133153f1e92c1
O.Kuloverova <oliich23@mail.ru>
2025-12-23 16:12:02 +0400

    add dist

---
+++ src/js/components/faqAccordion.js
@@ -0,0 +1,15 @@
+window.$ = window.jQuery = require('jquery');
+
+function faqAccordion() {
+  const items = document.querySelectorAll('.faq__item');
+  const infos = document.querySelectorAll('.faq__info');
+    if (items.length <= 0 || infos.length <= 0) return;
+  items.forEach((item, i) => {
+    item.addEventListener('click', () => {
+        item.classList.toggle('isActive');
+      $(infos[i]).slideToggle();
+    });
+  });
+}
+
+export default faqAccordion;
